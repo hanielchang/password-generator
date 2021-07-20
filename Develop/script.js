3516daf5d904f43392a237dddd759a3b2b6924c4
hanielchang <haniellloyd@gmail.com>
2021-07-19 22:28:38 -0700

    removed console logs and added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
-// Assignment code here
+// Lower case, upper case, and numbers arrays
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// For neatness, I chose to seperate the special characters by similarity (via their codenames) in 5 arrays. I then concatenated the 5 arrays into one
 const part1 = ["\u0020","\u0021","\u0022","\u0023","\u0024","\u0025","\u0026","\u0027","\u0028","\u0029"];
 const part2 = ["\u002A","\u002B","\u002C","\u002D","\u002E","\u002F"];
 const part3 = ["\u003A","\u003B","\u003C","\u003D","\u003E","\u003F"];
@@ -27,11 +29,12 @@ function generatePasswordContent() {
   var wantsUpperCase = window.confirm("Would you like upper case letters?");
   var wantsNumbers = window.confirm("Would you like numbers?");
   var wantsSpecialChars = window.confirm("Would you like special characters?");
-  console.log(wantsLowerCase, wantsUpperCase, wantsNumbers, wantsSpecialChars);
 
+  // Here we do a recursive call. Variable string holds the value when we convert to base case
   if (!wantsLowerCase && !wantsUpperCase && !wantsNumbers && !wantsSpecialChars) {
     window.alert("You must select at least one character type! Please try again.");
-    generatePasswordContent();
+    var string = generatePasswordContent();
+    return string;
   }
 
   if (wantsLowerCase){
@@ -50,28 +53,27 @@ function generatePasswordContent() {
     options = options.concat(specialChars);
   }
   
+  // Creating and verifying the password meets length requirements. Value received from function call.
   var passwordLength = generatePasswordLength();
-  console.log("Length is " + passwordLength);
   var password = '';
 
+  // This for loop generates the random password
   for (let count = 0; count < passwordLength; count++) {
     index = Math.floor(Math.random() * options.length);
-    console.log(index);
-
     randomChar = options[index]
     password = password.concat(randomChar);
   }
-  console.log(password);
-  console.log("String length is " + password.length);
+  return password;
 };
 
-var generatePasswordLength = function() {
+ function generatePasswordLength() {
   var length = window.prompt("How long would you like your password to be? Please choose between a length of 8 to 128 characters.");
   length = parseInt(length);
-  console.log("You chose " + length);
+  
+  // Here we do a recursive call, again with the variable 'number' to hold the value when we reach the base case 
   if ((length < 8) || (length > 128) || Number.isNaN(length)) {
     window.alert("Password does not meet length requirements. Please try again.");
-    number = generatePasswordLength();
+    var number = generatePasswordLength();
     return number;
   }
 
